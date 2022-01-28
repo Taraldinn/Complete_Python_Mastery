572e8e93f9d8d9fac4dcd2f3c5129601aa6de6ef
programmer-Quazi <kferdoush617@gmail.com>
2022-01-29 03:12:25 +0600

    ok done

--- app.py
+++ app.py
@@ -4,8 +4,14 @@ class Human:
     #Instance Method
     def Show_Name(self):
         print (self.name)
+    @classmethod
+    def cls_method(method):
+        print("My name is kazi mohammad aldin fardin")
 
 
 # human = Human("Nobita")
 # human.Show_Name()
-
+# human  = Human("Nobita")
+# human.cls_method()
+# Human.cls_method()
+Human.cls_method()
